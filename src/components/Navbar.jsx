ff61ddb25f4133f9d8cfe9a87b3279e5ccdb3d4c
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-10-22 06:55:55 +0700

    feat: done Navbar layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import Image from "next/image";
+import Logo from "../../public/logo.png";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [lastSection, setLastSection] = useState(0);
+  return (
+    <nav className="bg-orange-main flex px-8 justify-between">
+      <Link href="/" className="flex py-[9px] justify-center items-center w-fit gap-2">
+        <Image src={Logo} alt="Logo" className="w-[46px]" />
+        <h1 className="font-futura text-[32px]">Flash Ziswaf</h1>
+      </Link>
+
+      <div className="flex justify-center items-center font-inter text-[20px]">
+        <Link
+          href="#home"
+          className={
+            "flex justify-center items-center px-5 h-full " +
+            (lastSection == 0
+              ? "bg-white text-orange-main"
+              : "text-white bg-transparent")
+          }
+          onClick={() => setLastSection(0)}
+        >
+          Home
+        </Link>
+        <Link
+          href="#about"
+          className={
+            "flex justify-center items-center px-5 h-full " +
+            (lastSection == 1
+              ? "bg-white text-orange-main"
+              : "text-white bg-transparent")
+          }
+          onClick={() => setLastSection(1)}
+        >
+          About
+        </Link>
+        <Link
+          href="#pic"
+          className={
+            "flex justify-center items-center px-5 h-full " +
+            (lastSection == 2
+              ? "bg-white text-orange-main"
+              : "text-white bg-transparent")
+          }
+          onClick={() => setLastSection(2)}
+        >
+          PIC
+        </Link>
+        <Link
+          href="#lightning"
+          className={
+            "flex justify-center items-center px-5 h-full " +
+            (lastSection == 3
+              ? "bg-white text-orange-main"
+              : "text-white bg-transparent")
+          }
+          onClick={() => setLastSection(3)}
+        >
+          Lightning
+        </Link>
+        <Link
+          href="#news"
+          className={
+            "flex justify-center items-center px-5 h-full " +
+            (lastSection == 4
+              ? "bg-white text-orange-main"
+              : "text-white bg-transparent")
+          }
+          onClick={() => setLastSection(4)}
+        >
+          News
+        </Link>
+        <Link href="/auth/login" className="ml-5 rounded-full outline outline-white outline-[1px] px-6 py-2 italic font-semibold">
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+}
