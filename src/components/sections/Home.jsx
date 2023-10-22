71c0572f13c68d0c6b516ba61ab16f3e4f55f9b2
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-10-22 07:55:11 +0700

    feat: done Home section layout

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,124 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import Image from "next/image";
+import Home1 from "@/../public/home-1.png";
+import Home2 from "@/../public/home-2.png";
+import Home3 from "@/../public/home-3.png";
+import Home4 from "@/../public/home-4.png";
+import LightningNetwork from "@/../public/lightning-network.png";
+import "swiper/css";
+
+export default function HomeSection() {
+  return (
+    <section id="home" className="min-h-screen relative pb-[120px]">
+      <div className="top-0">
+        <Swiper
+          spaceBetween={0}
+          allowTouchMove={false}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: true,
+          }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          pagination={{ clickable: true }}
+          loop={true}
+          modules={[Autoplay, Navigation, Pagination]}
+        >
+          <SwiperSlide className="relative min-h-screen">
+            <Image
+              src={Home1}
+              className="absolute w-screen min-w-[920px] select-none"
+              alt="Home 1"
+            />
+            <SliderContent />
+          </SwiperSlide>
+          <SwiperSlide className="relative min-h-screen">
+            <Image
+              src={Home2}
+              className="absolute w-screen min-w-[920px] select-none"
+              alt="Home 2"
+            />
+            <SliderContent
+              headerTitle={"Donate to Mosque Construction"}
+              headerContent={
+                "Make a million prayers possible, one brick at a time."
+              }
+            />
+          </SwiperSlide>
+          <SwiperSlide className="relative min-h-screen">
+            <Image
+              src={Home3}
+              className="absolute w-screen min-w-[920px] select-none"
+              alt="Home 3"
+            />
+            <SliderContent 
+              headerTitle={"Donate to Orphans"}
+              headerContent={
+                "Be their hero. Be their hope. Be the change."
+              }
+            />
+          </SwiperSlide>
+          <SwiperSlide className="relative min-h-screen">
+            <Image
+              src={Home4}
+              className="absolute w-screen min-w-[920px] select-none"
+              alt="Home 4"
+            />
+            <SliderContent
+              headerTitle={"Donate to Disaster Relief"}
+              headerContent={
+                "Help rebuild and reconstruct society."
+              }
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
+}
+
+function SliderContent({headerTitle, headerContent}) {
+  return (
+    <div className="text-[32px] relative z-1 flex flex-col gap-[115px] justify-start pt-[200px] items-center min-h-screen font-inter">
+      <div>
+        <h1 className="text-[64px] px-[44px] text-shadow-orange text-orange-main font-bold text-center">
+          {headerTitle || "Donate to Palestinians"}
+        </h1>
+        <p className="text-center">
+          {headerContent || "Help our brethren to fight for their freedom."}
+        </p>
+      </div>
+
+      <div className="max-w-[900px] bg-white rounded-[60px] pb-[55px]">
+        <p className="w-full py-[60px] px-[44px] rounded-[60px] bg-orange-main text-center">
+          Then steadfastly continue to duly establish the Prayer, and
+          <span className="font-bold"> give the Zakât-Charity</span>.
+          <br />
+          <span className="italic">(Sûrat Al-Mujâdilah, 58:13)</span>
+        </p>
+        <div className="text-orange-main pt-[60px] pb-5 px-[44px] flex flex-col justify-center items-center gap-[60px]">
+          <p className="text-center max-w-[800px] text-[40px] mx-auto">
+            Faster and more transparent donations with
+            <span className="font-bold"> Lightning Network!</span>
+          </p>
+          <Image
+            src={LightningNetwork}
+            className="w-full"
+            alt="Lightning Network"
+          />
+          <div className="flex justify-evenly w-full font-bold">
+            <button className="bg-orange-main text-white rounded-full flex-shrink-0 py-5 px-20">
+              Learn Now
+            </button>
+            <button className="box-border text-orange-main border-[3px] border-orange-main rounded-full flex-shrink-0 py-5 px-20">
+              Donate Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import HomeSection from "@/components/sections/Home";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <HomeSection />
     </main>
   );
 }
