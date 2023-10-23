ea9358de2d77584f3f2b91dfa5c7a371108a505b
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-10-23 15:42:09 +0700

    fix: improve Home section responsiveness

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -82,25 +82,25 @@ export default function HomeSection() {
 
 function SliderContent({headerTitle, headerContent}) {
   return (
-    <div className="text-[32px] relative z-1 flex flex-col gap-[115px] justify-start pt-[200px] items-center min-h-screen font-inter">
+    <div className="text-[20px] md:text-[25px] xl:text-[32px] relative z-1 flex flex-col gap-[50px] md:gap-[80px] xl:gap-[115px] justify-start pt-[100px] md:pt-[150px] xl:pt-[200px] items-center min-h-screen font-inter">
       <div>
-        <h1 className="text-[64px] px-[44px] text-shadow-orange text-orange-main font-bold text-center">
+        <h1 className="text-[35px] md:text-[40px] xl:text-[64px] px-[44px] text-shadow-orange text-orange-main font-bold text-center">
           {headerTitle || "Donate to Palestinians"}
         </h1>
-        <p className="text-center">
+        <p className="text-center max-w-[90%] mx-auto">
           {headerContent || "Help our brethren to fight for their freedom."}
         </p>
       </div>
 
-      <div className="max-w-[900px] bg-white rounded-[60px] pb-[55px]">
-        <p className="w-full py-[60px] px-[44px] rounded-[60px] bg-orange-main text-center">
+      <div className="max-w-[90%] md:max-w-[600px] xl:max-w-[900px] overflow-hidden bg-white rounded-[40px] xl:rounded-[60px] pb-[55px]">
+        <p className="w-full py-[8%] px-[5%] md:py-[50px] md:px-[30px] xl:py-[60px] xl:px-[44px] rounded-[40px] xl:rounded-b-[60px] bg-orange-main text-center">
           Then steadfastly continue to duly establish the Prayer, and
           <span className="font-bold"> give the Zakât-Charity</span>.
           <br />
           <span className="italic">(Sûrat Al-Mujâdilah, 58:13)</span>
         </p>
-        <div className="text-orange-main pt-[60px] pb-5 px-[44px] flex flex-col justify-center items-center gap-[60px]">
-          <p className="text-center max-w-[800px] text-[40px] mx-auto">
+        <div className="text-orange-main pt-[60px] pb-5 px-[5%] md:px-[30px] xl:px-[44px] flex flex-col justify-center items-center gap-[60px]">
+          <p className="text-center max-w-[800px] text-[25px] md:text-[30px] xl:text-[40px] mx-auto">
             Faster and more transparent donations with
             <span className="font-bold"> Lightning Network!</span>
           </p>
@@ -109,11 +109,11 @@ function SliderContent({headerTitle, headerContent}) {
             className="w-full"
             alt="Lightning Network"
           />
-          <div className="flex justify-evenly w-full font-bold">
-            <button className="bg-orange-main text-white rounded-full flex-shrink-0 py-5 px-20">
+          <div className="flex flex-col xl:flex-row gap-5 justify-evenly w-full font-bold">
+            <button className="bg-orange-main text-white rounded-full flex-shrink-0 py-5 xl:px-20">
               Learn Now
             </button>
-            <button className="box-border text-orange-main border-[3px] border-orange-main rounded-full flex-shrink-0 py-5 px-20">
+            <button className="box-border text-orange-main border-[3px] border-orange-main rounded-full flex-shrink-0 py-5 xl:px-20">
               Donate Now
             </button>
           </div>
